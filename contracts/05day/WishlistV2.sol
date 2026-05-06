ef120828b37ea356cb01fd4ef9a96f431366fc01
alena-dev-soft <alenadev@outlook.com>
2026-05-06 09:07:29 +0700

    - rename file from day 4; - added listing for day 5

---
+++ contracts/05day/WishlistV2.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Wishlist { 
+    struct WishItem {
+        string name;
+        bool isFullfilled;
+        uint256 createdAt;
+    }
+
+    address public owner; 
+    
+    WishItem[] public wishes; 
+    
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addWish(string memory _name) public {
+        wishes.push(WishItem({
+            name: _name,
+            isFullfilled: false,
+            createdAt: block.timestamp
+        }));
+    }
+
+    function fulfillWish(uint _index) public {
+        require(msg.sender == owner, "Only owner");
+        require(_index < wishes.length, "Invalid index");
+        wishes[_index].isFullfilled = true;
+    }
+
+    function deleteWish(uint _index) public {
+        require(msg.sender == owner, "Only owner");
+        require(_index < wishes.length, "Invalid index");
+        wishes[_index] = wishes[wishes.length - 1];
+        wishes.pop();
+    }
+
+    function getWishes() public view returns (WishItem[] memory) {
+        return wishes;
+    }
+
+    function getWish(uint256 _index) public view returns(WishItem memory) {
+        require(_index < wishes.length, "Invalid index");
+        return wishes[_index];
+    }
+}
